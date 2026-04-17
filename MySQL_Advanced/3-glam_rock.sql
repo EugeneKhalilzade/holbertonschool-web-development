aefbb6d57cb12f669e0e5a9a262fa75a8d554adf
EugeneKhalilzade <elgynkhalilzade@gmail.com>
2026-04-17 15:41:59 +0000

    fix

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Lists all Glam rock bands ranked by longevity up to 2024
 SELECT band_name,
-       IF(split IS NULL, 0, IFNULL(NULLIF(split, ''), 2024) - formed) AS lifespan
+       IFNULL(NULLIF(split, ''), 2024) - formed AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', style) > 0
 ORDER BY lifespan DESC;
